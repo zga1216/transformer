b572072e26467bdc9d07d3c805d90aa89b8038ab
zga1216 <980368173@qq.com>
2025-09-18 10:51:33 +0800

    second commit

--- transfomer.py
+++ transfomer.py
@@ -102,3 +102,26 @@ class MultiHeadAttention(nn.Module):
         q = self.layer_norm(q)
 
         return q, attn  # 返回输出和注意力权重
+    
+    
+class PositionwiseFeedForward(nn.Module):
+    ''' A two-feed-forward-layer module '''
+
+    def __init__(self, d_in, d_hid, dropout=0.1):
+        super().__init__()
+        self.w_1 = nn.Linear(d_in, d_hid) # position-wise
+        self.w_2 = nn.Linear(d_hid, d_in) # position-wise
+        self.layer_norm = nn.LayerNorm(d_in, eps=1e-6)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+
+        residual = x
+
+        x = self.w_2(F.relu(self.w_1(x)))
+        x = self.dropout(x)
+        x += residual
+
+        x = self.layer_norm(x)
+
+        return x
